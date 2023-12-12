5638b3fc0fda3241667a20a0afce6631d18dc2ef
Brian Chen <enderchen004@outlook.com>
2023-12-12 23:13:15 +0000

    IB

---
+++ Task1B.py
@@ -0,0 +1,17 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+
+
+def run():
+    """Task 1B"""
+
+    stations = build_station_list()
+    a = stations_by_distance(stations, (52.2053, 0.1218))
+    b = []
+    for item in a:
+        b.append((item[0].name, item[0].town, item[1]))
+    print(b[:10], b[-10:], sep="\n\n\n")
+
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,12 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from .station import MonitoringStation
+from haversine import haversine, Unit
+
+def stations_by_distance(stations, p):
+    output = []
+    for station in stations:
+        output.append((station, haversine(station.coord, p)))
+    output = sorted_by_key(output, 1)
+    return output

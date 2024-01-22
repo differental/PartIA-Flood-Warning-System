671bfc5f2a41b8f1221f46cffd736dee2acd6203
Brian Chen <enderchen004@outlook.com>
2024-01-22 10:04:48 +0000

    change task 1d python file as requested

--- Task1D.py
+++ Task1D.py
@@ -1,5 +1,6 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_with_station, stations_by_river
+from floodsystem.utils import sorted_by_key
 
 def run():
     """Task 1D"""
@@ -9,8 +10,13 @@ def run():
     print(str(len(a)) + " stations. First 10 - " + str(sorted(list(a))[0:10]), sep="")
     river_names = ["River Aire", "River Cam", "River Thames"]
     dic = stations_by_river(stations)
+    
+    good_station_name = []
+    
     for river_name in river_names:
-        print(sorted(dic[river_name]))
+        for good_station in dic[river_name]:
+            good_station_name.append(good_station.name)
+        print(sorted(good_station_name))
 
 if __name__ == "__main__":
     print("*** Task 1D: CUED Part IA Flood Warning System ***")
